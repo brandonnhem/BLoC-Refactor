044616c26cb3a1454b6be9762c47192ae639dc93
brandonnhem <brandonnhem@gmail.com>
2020-04-20 22:04:09 -0700

    Added for separate widgets

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,5 @@
+export './add_passenger_button.dart';
+export './get_location_button.dart';
+export './loading_indicator.dart';
+export './passenger_list.dart';
+export './passenger_select_item.dart';
