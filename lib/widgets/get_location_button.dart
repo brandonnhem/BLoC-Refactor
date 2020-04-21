57f2a27494da8c8cb9b7e8b0447fba6d50864647
brandonnhem <brandonnhem@gmail.com>
2020-04-20 22:35:06 -0700

    Updated color and added blocbuilder

--- lib/widgets/get_location_button.dart
+++ lib/widgets/get_location_button.dart
@@ -1,20 +1,27 @@
+import 'package:addpassengers/blocs/blocs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class GetLocationButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        // color: _theme == 'Dark' ? Colors.amber : Colors.purple, TODO: add dark mode feature
-        shape: BoxShape.circle,
-      ),
-      child: IconButton(
-        icon: Icon(Icons.location_on),
-        onPressed: () {}, // TODO: add onPressed
-        tooltip: "Get Your Current Location",
-        iconSize: 36,
-        color: Colors.white,
-      ),
+    return BlocBuilder<PassengerBloc, PassengerState>(
+      builder: (context, state) { 
+        return Container(
+          decoration: BoxDecoration(
+            // color: _theme == 'Dark' ? Colors.amber : Colors.purple, TODO: add dark mode feature
+            color: Colors.purple,
+            shape: BoxShape.circle,
+          ),
+          child: IconButton(
+            icon: Icon(Icons.location_on),
+            onPressed: () {}, // TODO: add onPressed
+            tooltip: "Get Your Current Location",
+            iconSize: 36,
+            color: Colors.white,
+          ),
+        );
+      }
     );
   }
 }
